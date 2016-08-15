ca20ad9aa01b262bfd1a602502d7a8031e892fa9
Sandra Tsai <sandraytsai@gmail.com>
2016-08-15 17:55:25 +0000

    user controller and users/new view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815173317) do
+ActiveRecord::Schema.define(version: 20160815174714) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20160815173317) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
 end
