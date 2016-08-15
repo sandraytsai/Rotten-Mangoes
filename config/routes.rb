12b0acf2a1aa52afa6985996d77bbd3730b0904e
Sandra Tsai <sandraytsai@gmail.com>
2016-08-15 21:28:18 +0000

    routing root to movies#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  root to: 'movies#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
